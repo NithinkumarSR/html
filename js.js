e789fb7b658c07585cc1601e128e21ff8067c6e6
NithinkumarSR <cecsnithinkumarsr24@gmail.com>
2023-01-21 17:17:09 +0530

    js

---
+++ js.js
@@ -0,0 +1,18 @@
+'use strict'
+const a=10;
+const b=20;
+// console.log(a+b);
+//  console.log(20/10);
+//  console.log(10*b);
+//  const fristName='hi';
+//  const lastName='hello';
+//  console.log(fristName,lastName);
+//  console.log(`${fristName} java `);
+//  let x=10+5;
+//  x+=10;
+//  console.log(x);
+//  let  a=5;
+//  console.log(a**2);
+console.log(b<a);
+console.log(b>a);
+console.log(b>=20);
