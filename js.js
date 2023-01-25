86d0232e62dca3bc86caba0def87904f26b7d520
NithinkumarSR <cecsnithinkumarsr24@gmail.com>
2023-01-25 18:58:22 +0530

    new

--- js.js
+++ js.js
@@ -20,6 +20,7 @@
 // console.log(fristName+lastName);
 // console.log(fristName,lastName);
 // console.log(fristName+' '+lastName);
+     // problem
 // let weight=78;
 // let heigth=1.69;
 // let bmi=(weight/heigth)**2;
@@ -36,8 +37,57 @@
 // let bmi1=(weight1/heigth1)**2;
 // let a=bmi>bmi1;
 // console.log(a);
-const a='nithin \n\ kumar';
-console.log(a);
-const b=`hi
-hello`;
-console.log(b);
+           //string
+// const a='nithin\n\kumar';
+// console.log(a);
+// const b=`hi
+// hello`;
+// console.log(b);
+        //if condition
+//  const age=15;
+//  const x=age>=18;
+//  if(x){
+//     console.log(`age greater than ${age}`);
+//  }  
+//  else{
+//     console.log(`age less than 18`);
+//  }
+// const a=1000;
+// let b;
+// if(a>= 1000){
+//     b=1;
+// }
+// else{
+//  b=2;
+// }
+// console.log(b);
+           // problem
+// const student1= 'Mark';
+// const student2='John';
+// let mark1=80;
+// let mark2=90;
+// if(mark1<mark2){
+//     console.log(`'${student2}' BMI (${mark2})is higher than '${student1}'(${mark1})`);
+// }
+// else{
+//     console.log(`'${student1}' BMI (${mark1})is higher than '${student2}'(${mark2})`);
+// }
+// let a='1789';
+// console.log(Number(a)+1);
+// console.log(a+1);
+// a=Number(a);
+// console.log(a+2);
+// console.log(Number('hi'));
+// console.log(typeof NaN);
+// let b=1;
+// b=String(b);
+// console.log(typeof b);
+// console.log(Number(b),b);
+           // truthy and falsy
+let x;
+if(x){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
